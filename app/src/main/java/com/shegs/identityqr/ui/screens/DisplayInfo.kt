aff17005d2c99b83961e20ddda997551e5a60aa7
Oladokun oluwasegun <oladokunoluwasegun@gmail.com>
2023-08-28 15:10:45 +0100

    Fetching saved QR code

--- app/src/main/java/com/shegs/identityqr/ui/screens/DisplayInfo.kt
+++ app/src/main/java/com/shegs/identityqr/ui/screens/DisplayInfo.kt
@@ -1,16 +1,20 @@
 package com.shegs.identityqr.ui.screens
 
 import android.annotation.SuppressLint
+import android.graphics.BitmapFactory
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -89,6 +93,21 @@ fun DisplayInfoScreen(infoId: Int, viewModel: InformationViewModel) {
                 fontWeight = FontWeight.Bold,
                 modifier = Modifier.padding(bottom = 8.dp)
             )
+
+            // Display the QR code image if it's available in the card
+            card.qrCodeImage?.let { qrCodeImageByteArray ->
+                val imageBitmap = BitmapFactory.decodeByteArray(qrCodeImageByteArray, 0, qrCodeImageByteArray.size)?.asImageBitmap()
+                if (imageBitmap != null) {
+                    Image(
+                        bitmap = imageBitmap,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(200.dp) // Adjust the size as needed
+                            .padding(8.dp)
+                    )
+                }
+            }
+
         }
     }
 }
